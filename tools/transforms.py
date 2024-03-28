f25d7d8dd6dc4b8bf6c643818c2c72b17aeb52a3
FGarridoV <f.garridov@uc.cl>
2024-03-28 18:31:56 -0300

    less augmentation

--- tools/transforms.py
+++ tools/transforms.py
@@ -11,8 +11,8 @@ def get_transform(kind: str):
         return transforms.Compose([
                 transforms.Resize((224, 224)),
                 transforms.RandomHorizontalFlip(),
-                transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1),
-                transforms.RandomRotation(5),
+                transforms.ColorJitter(brightness=0.05, contrast=0.05, saturation=0.05, hue=0.05),
+                transforms.RandomRotation(2),
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
             ])
