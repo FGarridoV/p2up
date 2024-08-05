dba36e035374f70fe055cd00e0791b96ac73ee4f
FGarridoV <f.garridov@uc.cl>
2024-08-06 00:27:50 +0200

    applier

--- apply.py
+++ apply.py
@@ -19,6 +19,7 @@ encoder_layers = ast.literal_eval(model_hparams['encoder_layers']) if model_hpar
 projection_layers = ast.literal_eval(model_hparams['projection_layers']) if model_hparams['projection_layers'] != 'not' else None
 act_f_encoder = model_hparams['act_f_encoder']
 act_f_projection = model_hparams['act_f_projection']
+L2_norm = model_hparams['L2_norm']
 
 trainer = PlaceEmbeddingTrainer(name = None,
                                 use_gpu = True,
@@ -28,13 +29,14 @@ trainer = PlaceEmbeddingTrainer(name = None,
 trainer.set_place_data(data = f'municipalities/{municipality}_h3_10/spatial_units.pkl', batch_size = batch_size)
 
 # Define the model
-trainer.set_model_for_application(base_model = base_model,
+trainer.set_model_for_application(name = None,
+                                    base_model = base_model,
                                     img2vec_encoder_layers = img2vec_encoder_layers,
                                     pooling = pooling,
                                     encoder_layers = encoder_layers,
                                     projection_layers = projection_layers,
                                     act_f_encoder = act_f_encoder,
-                                    act_f_projection = act_f_projection)
+                                    act_f_projection = act_f_projection, L2_norm= L2_norm)
 
 # eval on the city
 modelpth = f'results/{trainer}_e{epoch_model}.pth'
